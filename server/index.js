e5291640dd624b0317d695c58ad380fae730748c
Diego de la Paz <usrext-diegopazgon@gmrcanarias.com>
2025-05-14 07:41:16 +0100

    Bellakeo

--- server/index.js
+++ server/index.js
@@ -7,7 +7,10 @@ dotenv.config();
 import routes from './routes.js';
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://wow-citronela.netlify.app',
+  methods: ['GET', 'POST']
+}));
 app.use(express.json());
 app.use('/api', routes);
 
